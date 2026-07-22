dc8773d5baacec1b83121b4fd9e3ba263a64c2b4
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-22 23:08:24 +0300

    feat: add HomePage nad RoomDetailPage routes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router";
+import RouterLayout from "@/shared/layout/RouterLayout";
 import HomePage from "@/pages/HomePage";
+import RoomDetailPage from "@/pages/HomePage/RoomDetailPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route element={<RouterLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rooms/:slug" element={<RoomDetailPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
