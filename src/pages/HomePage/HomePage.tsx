62ee54883ecf6e73aa6b1ff2a2f6bfe7a4610dad
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-22 23:12:20 +0300

    feat: add Contact Page on HomePage and remove Layout wrapper

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,18 @@
-import Layout from "@/shared/layout/Layout";
 import Hero from "./Hero";
 import History from "./History";
 import Photos from "./Photos";
+import Contact from "./Contact";
 import { RoomsPreview } from "@/features/rooms";
 
 const HomePage = () => {
   return (
-    <Layout>
+    <>
       <Hero />
       <RoomsPreview />
       <History />
       <Photos />
-    </Layout>
+      <Contact />
+    </>
   );
 };
 
